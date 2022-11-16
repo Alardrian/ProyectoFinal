936f390bed4a010bf7dbd58b9ef8215ce8a6b410
Adrian_Guinot <aguinotalonso@iessonferrer.net>
2022-11-16 18:28:11 +0100

    cartelera 16nov

---
+++ cartelera/categorias.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="categorias.css">
+    <title>Categorias</title>
+</head>
+<body>
+    
+    <div id="contenedorCategorias">
+        <h1>Cartelera</h1>
+        <ul>
+            <a href="peliculas.php"><li>Terror<img src="img/peniwais.jpg" alt=""></li></a>
+            <a href="peliculas.php"><li><img src="img/lloda.jpeg" alt="">Star wars</li></a>
+        </ul>  
+    </div>
+</body>
+</html>
---
+++ cartelera/peliculas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peliculas</title>
+    <link rel="stylesheet" href="peliculas.css">
+</head>
+<body> 
+
+<div class="pelicula">
+<h2>Titulo</h2>             <p class="votos">Votos: 0</p>
+<img src="img/peniwais.jpg" alt="">   <h3 class="sinopsisTitulo">Sinopsis</h3>    
+<p class="sinopsisTexto">Aqui ira la sipasdiasdasd  gasd</p>
+<p class="duracion">Duración: minutos</p>           <a href="">Ver ficha</a>
+
+
+</div>
+    
+</body>
+</html>
