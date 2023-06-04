f7ab93ffa41d20cd98f84541bb6bb50cbb0edf0c
Alardrian <adrianguinots4@hotmail.com>
2023-06-04 23:22:33 +0200

    sql

--- proyectoFinal/sql/alquilerCoches.sql
+++ proyectoFinal/sql/alquilerCoches.sql
@@ -5,19 +5,25 @@ CREATE DATABASE alquiler_de_coches;
 USE alquiler_de_coches;
 
 CREATE TABLE usuario (
-    id_usuario INT PRIMARY KEY,
-    nombre VARCHAR(50),
-    correo_electronico VARCHAR(50),
-    contrasena VARCHAR(50)
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(50),
+    clave VARCHAR(400),
+    perfil VARCHAR(50)
 );
 
 CREATE TABLE cliente (
-    id_cliente INT PRIMARY KEY,
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT,
+    nombre VARCHAR(200),
+    email VARCHAR(200),
     fecha_de_nacimiento DATE,
     numero_de_telefono VARCHAR(20),
     direccion VARCHAR(100),
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+    tarjeta_credito VARCHAR(500),
+    caducidad_tarjeta VARCHAR(500),
+    pin_tarjeta VARCHAR(500),
+    FOREIGN KEY (id_usuario)
+        REFERENCES usuario (id_usuario)
 );
 
 CREATE TABLE tipo_de_vehiculo (
@@ -26,48 +32,70 @@ CREATE TABLE tipo_de_vehiculo (
 );
 
 CREATE TABLE vehiculo (
-    id_vehiculo INT PRIMARY KEY,
+    id_vehiculo INT AUTO_INCREMENT PRIMARY KEY,
     id_tipo_de_vehiculo INT,
     marca VARCHAR(50),
     modelo VARCHAR(50),
     anio INT,
     capacidad INT,
-    transmision VARCHAR(20),
-    precio_por_dia DECIMAL(10,2),
-    FOREIGN KEY (id_tipo_de_vehiculo) REFERENCES tipo_de_vehiculo(id_tipo_de_vehiculo)
+    precio_dia DECIMAL(10 , 2 ),
+    FOREIGN KEY (id_tipo_de_vehiculo)
+        REFERENCES tipo_de_vehiculo (id_tipo_de_vehiculo)
 );
 
+
 CREATE TABLE reserva (
-    id_reserva INT PRIMARY KEY,
+    id_reserva INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT,
     id_vehiculo INT,
     fecha_de_inicio DATE,
     fecha_de_fin DATE,
-    costo_total DECIMAL(10,2),
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-    FOREIGN KEY (id_vehiculo) REFERENCES vehiculo(id_vehiculo)
+    costo_diario DECIMAL(10 , 2 ),
+    FOREIGN KEY (id_cliente)
+        REFERENCES cliente (id_cliente),
+    FOREIGN KEY (id_vehiculo)
+        REFERENCES vehiculo (id_vehiculo)
+);
+	
+CREATE TABLE seguro (
+    id_seguro INT AUTO_INCREMENT PRIMARY KEY,
+    id_reserva INT,
+    tipo_seguro VARCHAR(50),
+    costo_total DECIMAL(10 , 2 ),
+    fecha_de_inicio DATE,
+    fecha_de_fin DATE,
+    FOREIGN KEY (id_reserva)
+        REFERENCES reserva (id_reserva)
 );
 
 CREATE TABLE factura (
-    id_factura INT PRIMARY KEY,
+    id_factura INT AUTO_INCREMENT PRIMARY KEY,
     id_reserva INT,
     fecha_de_emision DATE,
-    subtotal DECIMAL(10,2),
-    impuestos DECIMAL(10,2),
-    total DECIMAL(10,2),
-    FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
+    subtotal DECIMAL(10 , 2 ),
+    impuestos DECIMAL(10 , 2 ),
+    total DECIMAL(10 , 2 ),
+    FOREIGN KEY (id_reserva)
+        REFERENCES reserva (id_reserva)
 );
 
-CREATE TABLE tipo_de_cliente (
-    id_tipo_de_cliente INT PRIMARY KEY,
-    tipo_de_cliente VARCHAR(50),
-    id_cliente INT,
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+CREATE TABLE listado_danyos_vehiculo (
+    id_listado INT PRIMARY KEY,
+    id_vehiculo INT,
+    descripcion_danyo VARCHAR(500),
+    posicion_danyo VARCHAR(500),
+	medida VARCHAR(50),
+    FOREIGN KEY (id_vehiculo)
+        REFERENCES vehiculo (id_vehiculo)
 );
 
-CREATE TABLE vip (
-    id_vip INT PRIMARY KEY,
-    id_cliente INT,
-    rebaja INT(20),
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
-);
+
+-- inserts
+
+INSERT INTO tipo_de_vehiculo VALUES(1, "compacto");
+INSERT INTO tipo_de_vehiculo VALUES(2, "SUV");
+INSERT INTO tipo_de_vehiculo VALUES(3, "Furgo");
+INSERT INTO vehiculo(id_tipo_de_vehiculo,marca,modelo,anio,capacidad,precio_dia) VALUES (1, "prueba", "modeloprueba", 1998, 5, 50);
+INSERT INTO vehiculo(id_tipo_de_vehiculo,marca,modelo,anio,capacidad,precio_dia) VALUES (1, "prueba2", "modeloprueba", 1998, 5, 50);
+INSERT INTO vehiculo(id_tipo_de_vehiculo,marca,modelo,anio,capacidad,precio_dia) VALUES (1, "prueba3", "modeloprueba", 1998, 5, 50);
+INSERT INTO vehiculo(id_tipo_de_vehiculo,marca,modelo,anio,capacidad,precio_dia) VALUES (2, "prueba4", "modeloprueba", 1998, 5, 50);
